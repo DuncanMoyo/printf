111d13c10e2212e812b315ae956d4d6eee381cc7
chiloanerk <chiloane.rk@gmail.com>
2023-03-27 18:54:48 +0200

    fix conflict

--- main.h
+++ main.h
@@ -13,13 +13,14 @@ int print_char(va_list args);
 int print_str(va_list args);
 int print_percent(va_list args);
 int print_binary(unsigned int n);
-=======
 /* print a character */
 int character_print(char c);
 /* print a string */
 int string_print(char *str);
 /* print a percent sign */
 int percent_print(void);
+=======
+
 >>>>>>> ec64b79 (test functions)
 
 #endif
