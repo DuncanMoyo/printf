0be3b8abe41f0178401aac88ff88c4f495b6bb30
chiloanerk <chiloane.rk@gmail.com>
2023-03-27 20:56:59 +0200

    betty

--- percent_print.c
+++ percent_print.c
@@ -1,7 +1,10 @@
 #include "main.h"
-
+/**
+ * percent_print - prints a percentage sign
+ * Return: void
+*/
 /* Print a percent sign */
-int percent_print(void)
+int	percent_print(void)
 {
-    return write(1, "%", 1); /* write a percent sign to stdout */
+	return (write(1, "%", 1)); /* write a percent sign to stdout */
 }
--- string_print.c
+++ string_print.c
@@ -1,13 +1,18 @@
 #include "main.h"
-
+/**
+ * string_print - prints a string to stdout
+ * @str: string to be printed
+ * Return: number of characters
+*/
 /* Print a string */
-int string_print(char *str)
+int	string_print(char *str)
 {
-    int len = 0;
-    while (*str)
-    {
-        len += write(1, str, 1); /* write each character to stdout */
-        str++; /* move to the next character in the string */
-    }
-    return len; /* return the total number of characters written */
+	int len = 0;
+
+	while (*str)
+	{
+		len += write(1, str, 1); /* write each character to stdout */
+		str++;                   /* move to the next character in the string */
+	}
+	return (len); /* return the total number of characters written */
 }
