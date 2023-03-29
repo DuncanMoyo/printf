1ce67aff34c1582e81c2dd0b7394a108e0ea791e
chiloanerk <chiloane.rk@gmail.com>
2023-03-29 16:38:39 +0200

    handle NULL pointers in the string_print function. Modified the string_print function to print the string '(null)' when passed a null pointer.

--- string_print.c
+++ string_print.c
@@ -9,6 +9,12 @@ int	string_print(char *str)
 {
 	int len = 0;
 
+	if (str == NULL)
+	{
+		return (write(1, "(null)", 6));
+		/* i return 6 because "(null)" is made up of 6 characters */
+	}
+
 	while (*str)
 	{
 		len += write(1, str, 1); /* write each character to stdout */
