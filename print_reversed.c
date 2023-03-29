4514c28deeabac1e22a1cb5d810d1b3fbd7b4e43
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-29 00:41:49 +0000

    Added back the _strlen function

--- print_reversed.c
+++ print_reversed.c
@@ -2,3 +2,11 @@
 
 /**
  * print_reversed - prints a string in reverse
+ * @str: the strinf to be reversed
+ * Return: the string in reverse
+ */
+
+int print_reversed(char *str)
+{
+	int i, len;
+}
